2e647db44b130cb28756c7b112154a19407f366f
Werner Glinka <werner@glinka.co>
2022-11-11 16:05:00 -0600

    added cities

---
+++ studio/schemas/contentTypes/cities.js
@@ -0,0 +1,66 @@
+import {FiGlobe} from "react-icons/fi";
+
+export default {
+  name: 'cities',
+  title: 'Cities',
+  type: 'document',
+  icon: FiGlobe,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    { 
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    { 
+      name: 'size',
+      title: 'Size',
+      type: 'number'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+    {
+      name: 'logo',
+      title: 'City Logo',
+      type: 'image',
+    },
+    {
+      name: 'wappen',
+      title: 'City Wappen',
+      type: 'image',
+    },
+    {
+      name: 'websiteLink',
+      title: 'Website Link',
+      type: 'link',
+    },
+    {
+      name: 'wikiLink',
+      title: 'Wiki Link',
+      type: 'link',
+    },
+    
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image'
+    }
+  }
+}
--- studio/schemas/contentTypes/index.js
+++ studio/schemas/contentTypes/index.js
@@ -4,3 +4,4 @@ export {default as page} from "./page";
 export {default as authors} from "./authors";
 export {default as navigation} from "./navigation";
 export {default as siteSettings} from "./siteSettings";
+export {default as cities} from "./cities";
