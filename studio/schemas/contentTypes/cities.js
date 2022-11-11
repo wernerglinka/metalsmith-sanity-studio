5eee97f2d9b0250aa743e4878fc9f40bbdea0d22
Werner Glinka <werner@glinka.co>
2022-11-11 16:30:11 -0600

    made description wysiwyg

--- studio/schemas/contentTypes/cities.js
+++ studio/schemas/contentTypes/cities.js
@@ -11,10 +11,42 @@ export default {
       title: 'Name',
       type: 'string'
     },
-    { 
-      name: 'description',
+    {
       title: 'Description',
-      type: 'textBlock'
+      name: 'description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'H4', value: 'h4'},
+            {title: 'Quote', value: 'blockquote'}
+          ],
+          lists: [{title: 'Bullet', value: 'bullet'}],
+          marks: {
+            decorators: [{title: 'Strong', value: 'strong'}, {title: 'Emphasis', value: 'em'}],
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
     },
     { 
       name: 'size',
@@ -34,6 +66,23 @@ export default {
       name: 'image',
       title: 'Image',
       type: 'image',
+      fields: [
+        {
+          title: 'Alternative Text',
+          name: 'alt',
+          type: 'string'
+        },
+        {
+          title: 'Caption',
+          name: 'caption',
+          type: 'string'
+        },
+        {
+          name: 'credits',
+          type: 'string',
+          title: 'Credits',
+        }
+      ]
     },
     {
       name: 'logo',
