3ceb3da995e78277eabcb2aa83c16cd37c776c78
Werner Glinka <werner@glinka.co>
2022-11-12 19:00:40 -0600

    added description field

--- studio/schemas/sectionBlocks/logosListBlock.js
+++ studio/schemas/sectionBlocks/logosListBlock.js
@@ -14,6 +14,39 @@ export default {
       type: 'string'
     },
     {
+      title: 'Desctiption',
+      name: 'description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          lists: [{title: 'Bullet', value: 'bullet'}],
+          // Marks let you mark up inline text in the block editor.
+          marks: {
+            // Decorators usually describe a single property – e.g. a typographic
+            // preference or highlighting by editors.
+            decorators: [{title: 'Strong', value: 'strong'}, {title: 'Emphasis', value: 'em'}],
+            // Annotations can be any object structure – e.g. a link or a footnote.
+            annotations: [
+              {
+                title: 'URL',
+                name: 'link',
+                type: 'object',
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    },
+    {
       name: "listSource",
       title: "List Source",
       type: "string",
